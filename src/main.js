386b62d10e0cdaeca22f8187786632c43b3f1618
Amin <amin.mohebbi.2010@gmail.com>
2022-03-15 23:34:40 +0330

    chore(Admin Panel): update dependencies

--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { createPinia } from "pinia";
+import Toast from "vue-toastification";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "vue-toastification/dist/index.css";
 
 import "bootstrap";
 
-const app = createApp(App);
+import "bootstrap/dist/js/bootstrap.min.js";
 
+const app = createApp(App);
+const toastOptions = {
+  position: "top-right",
+  closeOnClick: true,
+  pauseOnHover: true,
+  timeout: 5000,
+};
 app.use(router);
+app.use(createPinia());
+app.use(Toast, toastOptions);
 
 app.mount("#app");
