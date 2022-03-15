20bfbdad2d0a5163dd588d83e95d3ea42cea3ae5
Amin <amin.mohebbi.2010@gmail.com>
2022-03-15 23:39:31 +0330

    feat(Admin Panel): create custom component for rendering in Toast

---
+++ src/components/shared/AlertText.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <strong>{{ strongTitle }}</strong> {{ msg }}.
+  </div>
+</template>
+<script>
+export default {
+  name: "AlertText",
+  props: {
+    strongTitle: String,
+    msg: String,
+  },
+};
+</script>
