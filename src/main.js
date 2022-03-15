0f8e570f16ded9a12bd7e01c6acaf06e385343ee
Amin <amin.mohebbi.2010@gmail.com>
2022-03-16 01:14:08 +0330

    chore(Admin Panel): add piniaPersist

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { createPinia } from "pinia";
+import piniaPersist from "pinia-plugin-persist";
 import Toast from "vue-toastification";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "vue-toastification/dist/index.css";
@@ -11,14 +12,18 @@ import "bootstrap";
 import "bootstrap/dist/js/bootstrap.min.js";
 
 const app = createApp(App);
+
 const toastOptions = {
   position: "top-right",
   closeOnClick: true,
   pauseOnHover: true,
   timeout: 5000,
 };
+const pinia = createPinia();
+pinia.use(piniaPersist);
+
 app.use(router);
-app.use(createPinia());
+app.use(pinia);
 app.use(Toast, toastOptions);
 
 app.mount("#app");
