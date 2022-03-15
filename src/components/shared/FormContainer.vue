8426afe539d8a60bf782544abe5071d6996fb961
Amin <amin.mohebbi.2010@gmail.com>
2022-03-15 23:40:13 +0330

    feat(Admin Panel): update form contianer

--- src/components/shared/FormContainer.vue
+++ src/components/shared/FormContainer.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="formContainer">
-    <h1>{{ title }}</h1>
-    <slot></slot>
+    <div class="container">
+      <div class="col col-md-4 offset-md-4">
+        <div class="formContainer__jumbotron">
+          <h1 class="formContainer__title">{{ title }}</h1>
+          <slot></slot>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -17,5 +23,16 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.formContainer__jumbotron {
+  padding: 4rem 2rem;
+  margin-bottom: 2rem;
+  background-color: #e9ecef;
+  border-radius: 0.3rem;
+}
+
+.formContainer__title {
+  text-align: center;
 }
 </style>
