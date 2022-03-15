e91820afae707d12b3278131714df396a6f2780c
Amin <amin.mohebbi.2010@gmail.com>
2022-03-15 23:37:38 +0330

    feat(Admin Panel): create login form and login page
    implement login functionality
    redirect user after successful login
    save token in localStorage
    save user credentials in store
    validate form before submit

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,26 +1,102 @@
 <template>
   <FormContainer title="Login">
-    <button @click="submit">Test</button>
+    <form @submit.prevent="submit" class="form" novalidate>
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input
+          type="email"
+          class="form-control"
+          id="email"
+          aria-describedby="emailHelp"
+          v-model="email"
+        />
+        <div
+          :class="[
+            'invalid-feedback',
+            { 'd-block': initialEmptyField && v$.email.$error },
+          ]"
+        >
+          Required Field
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input
+          type="password"
+          class="form-control"
+          id="password"
+          aria-describedby="emailHelp"
+          v-model="password"
+        />
+        <div
+          :class="[
+            'invalid-feedback',
+            { 'd-block': initialEmptyField && v$.password.$error },
+          ]"
+        >
+          Required Field
+        </div>
+      </div>
+      <button class="btn btn-primary" type="submit">Login</button>
+    </form>
+    <p class="info">
+      Don't have account?
+      <router-link to="/register"><b>Register</b></router-link>
+    </p>
   </FormContainer>
 </template>
 <script>
+import useVuelidate from "@vuelidate/core";
+import { required, email } from "@vuelidate/validators";
 import FormContainer from "./shared/FormContainer.vue";
+
 export default {
   name: "LoginForm",
 
-  components: [FormContainer],
-
+  components: {
+    FormContainer,
+  },
+  setup() {
+    return {
+      v$: useVuelidate(),
+    };
+  },
   inject: ["login"],
   data() {
     return {
-      message: "Hello World",
+      initialEmptyField: false,
+      email: "",
+      password: "",
     };
   },
   methods: {
-    submit(evt) {
-      evt.preventDefault();
-      this.login("amin@snapp.express", "1234");
+    async submit() {
+      const result = await this.v$.$validate();
+      if (!result) {
+        this.initialEmptyField = true;
+        return;
+      }
+      this.login(this.email, this.password);
     },
+  },
+  validations() {
+    return {
+      email: { required, email },
+      password: { required },
+    };
   },
 };
 </script>
+
+<style>
+.form {
+  width: 100%;
+  display: flex;
+  flex-flow: column;
+}
+.info {
+  text-align: left;
+  margin-top: 1rem;
+  margin-bottom: 0;
+}
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,25 +1,59 @@
 <script>
-import LoginForm from "../components/LoginForm.vue";
-import API from "../http/apis";
+import { useToast } from "vue-toastification";
+import LoginForm from "@/components/LoginForm.vue";
+import API from "@/http/apis";
+import AlertText from "@/components/shared/AlertText.vue";
+import { useUser } from "@/store/user";
 
 export default {
   components: {
     LoginForm,
   },
+  setup() {
+    return {
+      toast: useToast(),
+      store: useUser(),
+    };
+  },
   methods: {
     login(email, password) {
-      const test = API.login({ email, password });
-
-      console.log(test);
-    },
-    register() {
-      console.log("register");
+      API.login({ email, password })
+        .then((res) => {
+          this.store.setUserCredential(res.user);
+          this.$router.push("/admin");
+        })
+        .catch((err) => {
+          if (err.response) {
+            this.toast.error({
+              component: AlertText,
+              props: {
+                strongTitle: "Login Failed!",
+                msg: "Username or password is invalid",
+              },
+            });
+          } else if (err.request) {
+            this.toast.error({
+              component: AlertText,
+              props: {
+                strongTitle: "Login Failed!",
+                msg: "Check your connection",
+              },
+            });
+          } else {
+            this.toast.error({
+              component: AlertText,
+              props: {
+                strongTitle: "Login Failed!",
+                msg: "Unknown Error",
+              },
+            });
+          }
+        });
     },
   },
   provide() {
     return {
       login: this.login,
-      register: this.register,
     };
   },
 };
