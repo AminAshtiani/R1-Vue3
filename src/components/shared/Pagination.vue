588d74b03e8b94194abd081798f475f36d5adca7
Amin <amin.mohebbi.2010@gmail.com>
2022-03-16 22:27:06 +0330

    feat(Admin Panel): create Pagination component

---
+++ src/components/shared/Pagination.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav v-if="totalPage > 1">
+    <ul class="pagination">
+      <li
+        v-for="index in totalPage"
+        :key="index"
+        class="page-item"
+        :class="{ active: currentPage == index }"
+        @click="updateCurrentPage(index)"
+      >
+        <span class="page-link">
+          {{ index }}
+        </span>
+      </li>
+    </ul>
+  </nav>
+</template>
+<script>
+import { ref } from "vue";
+export default {
+  name: "Pagination",
+  props: {
+    totalPage: Number,
+  },
+  setup(props, ctx) {
+    const currentPage = ref(1);
+    function updateCurrentPage(page) {
+      currentPage.value = page;
+
+      ctx.emit("clicked", currentPage.value)
+    }
+    return {
+      currentPage,
+      updateCurrentPage,
+    };
+  },
+};
+</script>
