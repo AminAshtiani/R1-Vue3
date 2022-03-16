b7d5c122e3d9f90d4b54f40f61a3fd5385116c6d
Amin <amin.mohebbi.2010@gmail.com>
2022-03-16 22:26:46 +0330

    feat(Admin Panel): creates Modal component

---
+++ src/components/shared/Modal.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="modal fade" tabindex="-1" role="dialog" id="example">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">{{ title }}</h5>
+          <button
+            type="button"
+            class="close"
+            data-dismiss="modal"
+            aria-label="Close"
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p>{{ message }}</p>
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-primary"
+            data-dismiss="modal"
+            @click="$emit('confirm')"
+          >
+            Save changes
+          </button>
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">
+            Close
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Modal",
+  props: {
+    title: String,
+    message: String,
+  },
+};
+</script>
